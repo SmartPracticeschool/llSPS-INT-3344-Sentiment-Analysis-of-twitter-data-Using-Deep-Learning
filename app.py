c7a0e8ed4d60732c0162808882f113893b15e0c1
Tushar Upadhyay <47355104+tushar-upadhyay@users.noreply.github.com>
2020-07-08 22:58:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 
 from flask import Flask, request, jsonify, render_template
-from brain import brain
+# from brain import brain
+import random
 app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-
 @app.route('/y_predict',methods=['POST'])
 def y_predict():
     sentiment = request.form["Message"]
-    return render_template('index.html',result=brain(sentiment))
+    return render_template('index.html',result=random.randint(0,1))
 if __name__=="__main__":
     app.run()
 
