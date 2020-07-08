7ff7615dbbbcf29d14edc8705f9cfb97e8965a2f
Suraj <48567316+real4suraj2@users.noreply.github.com>
2020-07-08 23:11:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from brain import brain
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', result = '')
 
 @app.route('/y_predict',methods=['POST'])
 def y_predict():
     sentiment = request.form["Message"]
-    return render_template('index.html',result=brain(sentiment))
+    return render_template('index.html', result = brain(sentiment))
     #return render_template('index.html', result = str(brain(sentiment)) + '.jpg') 
 if __name__ == "__main__":
     app.run()
