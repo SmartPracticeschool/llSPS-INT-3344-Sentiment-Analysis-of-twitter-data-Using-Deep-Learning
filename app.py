4ee6439a4917ce238d699fd2edf653b521825824
Suraj <48567316+real4suraj2@users.noreply.github.com>
2020-07-09 22:11:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from brain import brain
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('index.html')
+    err = brain(sentiment)
+    if err == 0 or err == 1 : err = ''
+    return render_template('index.html', result = err)
 @app.route('/',methods=['POST'])
 def y_predict():
     sentiment = request.form["Message"]
