9030f8c7961a4b932d05da336c00afa9c6868483
tushar-upadhyay <tusharrockpg@gmail.com>
2020-07-09 17:05:21 +0530

    project completed

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/y_predict',methods=['POST'])
+@app.route('/',methods=['POST'])
 def y_predict():
     sentiment = request.form["Message"]
-    #return render_template('index.html',result=random.randint(0,1))
-    return render_template('index.html', result = str(brain(sentiment)) + '.jpg')
+    return render_template('index.html', result = str(brain(sentiment)) + '.png')
 if __name__=="__main__":
     app.run()
 
