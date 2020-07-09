908e3a738c07f24ae43c7fed68dbb98c1b08a9b8
Suraj <48567316+real4suraj2@users.noreply.github.com>
2020-07-09 22:17:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ app = Flask(__name__)
 def home():
     err = "Saved Model Doesn't Exist"
     if os.path.isfile('./model.h5'): err = ''
-    return render_template('index.html', result = err)
+    return render_template('index.html', result = '', err = err)
 @app.route('/',methods=['POST'])
 def y_predict():
     sentiment = request.form["Message"]
-    return render_template('index.html', result = str(brain(sentiment)) + '.png')
+    err = str(brain(sentiment))
+    if err == 0 or err == 1 : err = ''
+    return render_template('index.html', result = err + '.png', err = err)
 if __name__=="__main__":
     app.run()
 
