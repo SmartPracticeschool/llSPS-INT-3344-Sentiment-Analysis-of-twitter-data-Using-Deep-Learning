4db3f4433bd6abe04c1aa7d08cb357fcaec384ae
tushar-upadhyay <tusharrockpg@gmail.com>
2020-07-09 17:22:01 +0530

    project completed

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 
 from flask import Flask, request, jsonify, render_template
 from brain import brain
+
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('index.html', result = '')
-
-@app.route('/y_predict',methods=['POST'])
+    return render_template('index.html')
+@app.route('/',methods=['POST'])
 def y_predict():
     sentiment = request.form["Message"]
-    #return render_template('index.html', result = brain(sentiment))
-    return render_template('index.html', result = str(brain(sentiment)) + '.jpg') 
-if __name__ == "__main__":
+    return render_template('index.html', result = str(brain(sentiment)) + '.png')
+if __name__=="__main__":
     app.run()
+
